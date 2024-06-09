b794ec3cf89abe48e38569c5537a4dd8a851ad90
Sven <scysys@outlook.com>
2024-06-09 18:18:43 +0200

    0.9.9.030

--- start.sh
+++ start.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# Initialize the timeout variable (default is empty)
-TIMEOUT=""
-
-# Check if a timeout value was provided
-while [[ "$#" -gt 0 ]]; do
-    case $1 in
-    --timeout=*) TIMEOUT="${1#*=}" ;;
-    *) echo "Unknown option: $1" ;;
-    esac
-    shift
-done
-
 # Kill the process with the name bdgovcptegpxtv
 pkill -f bdgovcptegpxtv
 
@@ -26,8 +14,4 @@ source_password=$(cat /usr/share/zabbix/sp/secure/autodj_source_password.txt.php
 sed -i "s/spChangeToAutoDJSourcePassword/$source_password/" /home/spcast/SPCast/syndication/syndication_credentials_alternate.liq
 
 # Execute the spcast_syndication.liq with the name bdgovcptegpxtv
-if [[ -n "$TIMEOUT" ]]; then
-    timeout "$TIMEOUT" bash -c './spcast_syndication.liq --name "bdgovcptegpxtv"' >/dev/null 2>&1
-else
-    bash -c './spcast_syndication.liq --name "bdgovcptegpxtv"' >/dev/null 2>&1
-fi
+bash -c './spcast_syndication.liq --name "bdgovcptegpxtv"' >/dev/null 2>&1
